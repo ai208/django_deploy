be6f5ccb6fe880cf2478c202f387d6c747bbcd36
ai208 <yasu6328hand@gmail.com>
2026-02-13 21:44:13 +0900

    healthrecord 動作確認

--- liveapp/healthapp/models.py
+++ liveapp/healthapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+class HealthRecord(models.Model):
+     User= get_user_model()
+     user = models.ForeignKey(User,on_delete=models.CASCADE)
+     date = models.DateField(auto_now_add=True)
+     CONDITION_CHOICES = [(i,str(i)) for i in range(1,6)]
+     condition = models.IntegerField(choices=CONDITION_CHOICES,default=3,verbose_name="体調")
+     mental_status = models.IntegerField(choices=CONDITION_CHOICES,default=3,verbose_name="精神状態")
+     wakeup_time = models.DateTimeField(null=True,blank=True,verbose_name="起床時間")
+     bed_time = models.DateTimeField(null=True,blank=True,verbose_name="就寝時間")
+     sleep_duration = models.FloatField(null=True,blank=True,verbose_name="睡眠時間(h)")
+     class Meta:
+          ordering = ['-date']
+          constraints = [
+               models.UniqueConstraint(
+                    fields=['user','date'],
+                    name = 'unique_user_daily_record'
+               )
+          ]
