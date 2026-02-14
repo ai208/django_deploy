90ab6d6dd448a4f5436473943ce0b710468ba939
ai208 <yasu6328hand@gmail.com>
2026-02-14 18:57:57 +0900

    ラベルの変更

--- liveapp/healthapp/forms.py
+++ liveapp/healthapp/forms.py
@@ -8,7 +8,7 @@ class HealthRecordForm(forms.ModelForm):
         labels ={
             'condition':'体調',
             'mental_status':'精神状態',
-            'weight':'体調',
+            'weight':'体重(kg)',
             'wakeup_time':'起床時間',
             'bed_time':'就寝時間(編集可能)',
             'sleep_duration':'睡眠時間(h)',
