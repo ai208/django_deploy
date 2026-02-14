49ab305f0e73ce5047becbf96d38a48a1f4af68e
ai208 <yasu6328hand@gmail.com>
2026-02-14 12:11:28 +0900

    permisson削除

--- liveapp/healthapp/views.py
+++ liveapp/healthapp/views.py
@@ -3,7 +3,7 @@ from. import models
 from django.views.generic import ListView, DetailView, CreateView,UpdateView,DeleteView
 from django.urls import reverse_lazy
 from .forms import HealthRecordForm
-from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
@@ -21,28 +21,25 @@ class HealthRecordDetailView(LoginRequiredMixin,DetailView):
     def get_queryset(self):
         return models.HealthRecord.objects.filter(user=self.request.user)
 
-class HealthRecordCreateView(PermissionRequiredMixin,LoginRequiredMixin,CreateView):
+class HealthRecordCreateView(LoginRequiredMixin,CreateView):
     model = models.HealthRecord
     template_name = 'healthapp/healthrecord_create.html'
     form_class = HealthRecordForm
     success_url = reverse_lazy('healthrecord_list')
-    permission_required = 'healthapp.add_healthrecord'
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
-class HealthRecordUpdateView(UpdateView,PermissionRequiredMixin,LoginRequiredMixin):
+class HealthRecordUpdateView(UpdateView,LoginRequiredMixin):
     model = models.HealthRecord
     template_name = 'healthapp/healthrecord_update.html'
     form_class = HealthRecordForm
     success_url = reverse_lazy('healthrecord_list')
-    permission_required = 'healthapp.add_healthrecord'
     def get_queryset(self):
         return models.HealthRecord.objects.filter(user=self.request.user)
-class HealthRecordDeleteView(DeleteView,PermissionRequiredMixin,LoginRequiredMixin):
+class HealthRecordDeleteView(DeleteView,LoginRequiredMixin):
     model = models.HealthRecord
     template_name = 'healthapp/healthrecord_delete.html'
     success_url = reverse_lazy('healthrecord_list')
-    permission_required = 'healthapp.add_healthrecord'
     def get_queryset(self):
         return models.HealthRecord.objects.filter(user=self.request.user)
